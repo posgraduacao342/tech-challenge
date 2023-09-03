32474bbf57b156fdf8efaf8a0861fc24c798391d
Lucas Tadeu <lucasu37@gmail.com>
2023-09-03 20:23:22 -0300

    feat: implmentado os value objects de cpf e email.

--- src/main/java/api/techchallenge/application/presenters/mappers/ClienteMapper.java
+++ src/main/java/api/techchallenge/application/presenters/mappers/ClienteMapper.java
@@ -1,7 +1,10 @@
 package api.techchallenge.application.presenters.mappers;
 
 import api.techchallenge.application.presenters.requests.cliente.CriarClienteRequest;
+import api.techchallenge.application.presenters.responses.cliente.CriarClienteResponse;
 import api.techchallenge.domain.entities.Cliente;
+import api.techchallenge.domain.valueObjects.CPF;
+import api.techchallenge.domain.valueObjects.Email;
 import api.techchallenge.infrastructure.db.entity.ClienteEntity;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
@@ -13,15 +16,53 @@ public class ClienteMapper {
         BeanUtils.copyProperties(clienteRequest, cliente);
         return cliente;
     }
+
     public Cliente toDomain(ClienteEntity clienteEntity) {
         var cliente = new Cliente();
         BeanUtils.copyProperties(clienteEntity, cliente);
         return cliente;
     }
 
-    public ClienteEntity toEntity(Cliente cliente){
+    public ClienteEntity toEntity(Cliente cliente) {
         var clienteEntity = new ClienteEntity();
         BeanUtils.copyProperties(cliente, clienteEntity);
         return clienteEntity;
+    }
+
+    public ClienteEntity clienteToClienteEntity(Cliente cliente) {
+        var clienteEntity = new ClienteEntity();
+        clienteEntity.setNome(cliente.getNome());
+        clienteEntity.setEmail(String.valueOf(cliente.getEmail()));
+        clienteEntity.setCpf(String.valueOf(cliente.getCpf()));
+
+        clienteEntity.setDataAtualizacao(cliente.getDataAtualizacao());
+        clienteEntity.setDataCriacao(cliente.getDataCriacao());
+        return clienteEntity;
+    }
+
+    public Cliente clienteEntityToCliente(ClienteEntity clienteEntity) {
+        var cliente = new Cliente();
+        var email = new Email(clienteEntity.getEmail());
+        var cpf = new CPF(clienteEntity.getCpf());
+        cliente.setId(clienteEntity.getId());
+        cliente.setNome(clienteEntity.getNome());
+        cliente.setEmail(email);
+        cliente.setCpf(cpf);
+        cliente.setDataAtualizacao(clienteEntity.getDataAtualizacao());
+        cliente.setDataCriacao(clienteEntity.getDataCriacao());
+        return cliente;
+    }
+
+    public CriarClienteResponse clienteToCriarClienteResponse(Cliente cliente) {
+        var response = new CriarClienteResponse();
+        response.setDataCriacao(cliente.getDataCriacao());
+        response.setDataDelecao(cliente.getDataDelecao());
+        response.setDataAtualizacao(cliente.getDataAtualizacao());
+        response.setId(cliente.getId());
+        response.setNome(cliente.getNome());
+        response.setEmail(String.valueOf(cliente.getEmail()));
+        response.setCpf(String.valueOf(cliente.getCpf()));
+        response.setPedidos(cliente.getPedidos());
+        return response;
     }
 }
--- src/main/java/api/techchallenge/application/presenters/requests/cliente/CriarClienteRequest.java
+++ src/main/java/api/techchallenge/application/presenters/requests/cliente/CriarClienteRequest.java
@@ -1,10 +1,8 @@
 package api.techchallenge.application.presenters.requests.cliente;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.validator.constraints.br.CPF;
 
 @Getter
 @Setter
@@ -13,10 +11,8 @@ public class CriarClienteRequest {
     private String nome;
 
     @NotBlank
-    @Email
     private String email;
 
     @NotBlank
-    @CPF
     private String cpf;
 }
